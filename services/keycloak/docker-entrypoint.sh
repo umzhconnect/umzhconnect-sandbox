6037f678e9d62210720d1fc6b146f4b4f8e36d1c
Ben-Herz <140254229+Ben-Herz@users.noreply.github.com>
2026-06-12 14:43:58 +0200

    Fix Keycloak realm import permissions using named volume

--- services/keycloak/docker-entrypoint.sh
+++ services/keycloak/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 sed "s|\${WEB_APP_PORT}|${WEB_APP_PORT}|g" \
-    /opt/keycloak/data/import/realm-export.json.template \
+    /tmp/realm-template.json \
     > /opt/keycloak/data/import/realm-export.json
 exec /opt/keycloak/bin/kc.sh "$@"
